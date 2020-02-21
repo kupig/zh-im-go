815dbfd74d5eaa837ab28ce14f14a00f7a64fe96
yan <447265629@qq.com>
2020-02-21 17:56:57 +0800

    增加服务器作为客户端类型，用于消息分发

--- public/config/config.go
+++ public/config/config.go
@@ -5,6 +5,13 @@ const (
 	TCPServerAdrress = "127.0.0.1:8899"
 )
 
+// 客户端类型
+const (
+	WORLD_CLI = 1 // 世界服务器作为客户端
+	CONN_CLI  = 2 // 连接服务器作为客户端
+	LOGIC_SVR = 3 // 逻辑服务器作为客户端
+)
+
 // 服务器类型
 const (
 	WORLD_SVR = 1 // 世界服务器
